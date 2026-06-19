27dc9128f947f80e51712b7b6ff92685b60758e2
Zamie <zamie693@gmail.com>
2026-06-20 04:06:32 +0700

    style: toggle between full logo image and icon depending on sidebar collapse state

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,14 +3,17 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
+    <!-- Full logo shown when expanded -->
+    <img
+        src="/logo_superposko.png"
+        alt="SuperPosko"
+        class="h-8 w-auto object-contain block group-data-[state=collapsed]:hidden"
+    />
+
+    <!-- Icon logo shown when collapsed -->
     <div
-        class="flex aspect-square size-8 items-center justify-center rounded-md"
+        class="hidden group-data-[state=collapsed]:flex aspect-square size-8 items-center justify-center rounded-md"
     >
         <AppLogoIcon class="size-8 object-contain" />
-    </div>
-    <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate leading-tight font-semibold"
-            >SuperPosko</span
-        >
     </div>
 </template>
