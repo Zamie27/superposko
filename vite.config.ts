bc8ed6618279b534382177768d783d976553268b
Zamie <zamie693@gmail.com>
2026-06-19 20:27:22 +0700

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -31,4 +31,14 @@ export default defineConfig({
             formVariants: true,
         }),
     ],
+    build: {
+        rollupOptions: {
+            onwarn(warning, warn) {
+                if (warning.code === 'INVALID_ANNOTATION' && warning.message.includes('node_modules')) {
+                    return;
+                }
+                warn(warning);
+            },
+        },
+    },
 });
