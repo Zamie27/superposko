ca687561aa50e7b4e107edd94316b2fca07bf0f6
Zamie27 <zamie693@gmail.com>
2026-07-22 04:08:30 +0700

    fix: import Storage facade in AttendanceController

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -7,6 +7,7 @@ use App\Models\Setting;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Inertia\Inertia;
 use Inertia\Response;
